b0f1844fe23b847306fb039f0898681685b4591c
Andrew Panasyuk <avpk77@gmail.com>
2015-11-19 12:04:22 +0200

    f3

--- Launcher.java
+++ Launcher.java
@@ -11,41 +11,11 @@ public class Launcher {
 //        SplashScreen splashScreen = SplashScreen.getSplashScreen();
 //        Thread.sleep(5000);
 //        splashScreen.close();
-        //filds.ControlPanel cp = new filds.ControlPanel();
-//        System.out.println(Thread.activeCount());
-//        Thread[]threads = new Thread[Thread.activeCount()];
-//        Thread.enumerate(threads);
-//        for (Thread t: threads){
-//        w    System.out.println(t);
-//        }
-        //control.Move m = new control.Move();
-
-//        List<service.Action> actionList = new ArrayList<>();
-//        actionList.add(service.Action.DOWN);
-//        actionList.add(service.Action.FIRE);
-//        System.out.println(actionList.get(0));
-//        System.out.println(actionList.contains(service.Action.DOWN));
 
 
         ActionField actionField = new ActionField();   //*****************************************
 
-//        stack.removeFirst();
-//        System.out.println(stack.getFirst());
-//        for (Iterator<String> i: stack.){
-//
-//        }
-
-
-//        for (int i = 0; i < service.Direction.values().length; i++) {
-//            System.out.println(service.Direction.values()[i]);
-//
-//        }
-//        System.out.println("++");
-//        for (int i = 0; i < service.Action.values().length; i++) {
-//            System.out.println(service.Action.values()[i]);
-//
-//        }
-        //actionField.getQadrant(1, 1);
+//        
 
 
 
